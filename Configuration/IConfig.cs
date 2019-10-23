556f86cb58013b8e9050d72a562ab4ae6268c39d
John Hyland <john@parrismotion.com>
2019-10-23 12:48:44 -0700

    ..

---
+++ Configuration/IConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Rpi.Health;
+
+namespace Rpi.Configuration
+{
+    public interface IConfig
+    {
+        IConfigurationRoot Root { get; }
+        bool IsWindows { get; }
+        bool IsLinux { get; }
+        bool IsOSX { get; }
+        string DeviceSerial { get; }
+        string DeviceName { get; set; }
+        string ServiceName { get; }
+        string ServiceVersion { get; }
+        string ApplicationPath { get; }
+        int ListenPort { get; }
+        TimeSpan ErrorRetention { get; }
+        string[] InterfaceNames { get; }
+        int PollingIntervalMs { get; }
+        int StickyHighInputMs { get; }
+        Interface PrimaryInterface { get; set; }
+        
+        string GetOS();
+        int GetMegabytesUsedByProcess();
+    }
+}
