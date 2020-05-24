9963fd74da9976a6b629a230916d58c93ca34b73
John Hyland <jonhyland@hotmail.com>
2020-05-24 13:07:40 -0700

    ..

--- scripts/install_dnsmasq.sh
+++ scripts/install_dnsmasq.sh
@@ -12,3 +12,10 @@ set -e
 
 # vars
 HOME="/home/pi"
+
+# install dnsmasq
+if [ $(dpkg-query -W -f='${Status}' dnsmasq 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+    echo "Installing dnsmasq.."
+    apt-get install dnsmasq
+fi
