248c83c5e818f7d00e91490d54aa8d1de427cb69
John Hyland <jonhyland@hotmail.com>
2020-05-24 14:51:45 -0700

    ..

---
+++ scripts/restart_dnsmasq.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ensure run as root
+if [ "$EUID" -ne 0 ]
+then
+    echo "Must be run as root"
+    exit
+fi
+
+# break on error
+set -e
+
+# restart service
+systemctl restart dnsmasq
